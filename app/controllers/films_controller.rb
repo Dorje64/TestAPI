94e65501d0ad397d71b6d2fd744599c09ea5ab75
Dorje <ku.dorjee@gmail.com>
2016-12-07 12:40:53 +0545

    Re-factored code and managed indentation.

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,7 +2,7 @@ require "csv"
 class FilmsController < ApplicationController
 	def index
 		@films = CSV.read('Film.csv')
-		@film = @films[rand(1..@films.count)]
+		@film = @films[rand(1...@films.count)]
 
 		respond_to do |format|
 	  	data = { :title => @film[1], :year=>@film[2]}
@@ -10,6 +10,5 @@ class FilmsController < ApplicationController
 	  	format.html  { render 'index'}
 	  	format.xml { render :xml => data }
 	  end
-
 	end
 end
